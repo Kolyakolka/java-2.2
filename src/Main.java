09d972a1cb2966912c74c5aeffcf944f0732d933
Kolyakolka <avr.var.02@mail.ru>
2022-06-23 21:15:41 +0500

    new1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 100;
+        int payment = 1001;
+        int bonus;
+
+        if (payment > 1000) {
+            bonus = payment / 100;
+        } else {
+            bonus = 0;
+        }
+
+        String smile;
+        if (bonus == 0) {
+            smile = " =(";
+        } else {
+            smile = " =)";
+        }
+
+        int balance = account + payment + bonus;
+        System.out.println("Hello, thank you for your payment. Your balance is: " + balance + " RUB Your bonus is : " + bonus + " RUB." + smile);
+    }
+}
